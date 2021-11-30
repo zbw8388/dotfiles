cfc2b274b64eaf45538bfe5d8ae2d43de1d86fec
Dominic Martinez <dominic.martinez@duke.edu>
2021-11-30 16:42:57 -0500

    guix system-d config

--- system/configuration.nix
+++ system/configuration.nix
@@ -10,7 +10,22 @@
       # Include system accounts
       ../.secrets/system-accounts.nix
     ];
-  
+
+    systemd.services.guix-daemon = {
+      enable = true;
+      description = "Build daemon for GNU Guix";
+      serviceConfig = {
+        ExecStart="/var/guix/profiles/per-user/root/current-guix/bin/guix-daemon --build-users-group=guixbuild";
+        Environment="GUIX_LOCPATH=/var/guix/profiles/per-user/root/guix-profile/lib/locale";
+        RemainAfterExit="yes";
+
+        # See <https://lists.gnu.org/archive/html/guix-devel/2016-04/msg00608.html>.
+        # Some package builds (for example, go@1.8.1) may require even more than
+        # 1024 tasks.
+        TasksMax="8192";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   # And allow proprietary software to be installed
   nixpkgs.config.allowUnfree = true;
 
